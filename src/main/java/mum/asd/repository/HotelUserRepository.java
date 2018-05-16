f7cf16a0a588949f2ff3ea17c6ccf7168c75583d
Tho Nguyen <nguyentantho1302@gmail.com>
2018-05-16 15:40:25 -0500

    Register

--- src/main/java/mum/asd/repository/HotelUserRepository.java
+++ src/main/java/mum/asd/repository/HotelUserRepository.java
@@ -1,16 +1,9 @@
 package mum.asd.repository;
 
 import mum.asd.domain.HotelUser;
-<<<<<<< HEAD
 import mum.asd.domain.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
-
-@Repository
-public interface HotelUserRepository extends JpaRepository<HotelUser, Long> {
-
-	User findByEmail(String email);
-=======
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
@@ -19,11 +12,6 @@ import org.springframework.stereotype.Repository;
 import java.util.List;
 
 @Repository
-public interface HotelUserRepository extends JpaRepository<HotelUser, Integer> {
-//    @Query("SELECT distinct h from HotelUser h WHERE h.firstName = :firstName")
-//    List<HotelUser> findHotelUserByFirstName(@Param("firstName") String firstName);
-
-//    List<HotelUser>findHotelUserByFirstName(String fistName);
-
->>>>>>> e4c8016e07f18ff9494c64f7ae7941a3ab9eab91
+public interface HotelUserRepository extends JpaRepository<HotelUser, Long> {
+	User findByEmail(String email);
 }
